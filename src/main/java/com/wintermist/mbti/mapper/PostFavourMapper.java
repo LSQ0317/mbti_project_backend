fce8fdc5c6f87481de4b989de605287d0d657763
liushaoqi <wintermist317@gmail.com>
2026-03-03 14:23:44 +0800

    project init

---
+++ src/main/java/com/wintermist/mbti/mapper/PostFavourMapper.java
@@ -0,0 +1,35 @@
+package com.wintermist.mbti.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wintermist.mbti.model.entity.Post;
+import com.wintermist.mbti.model.entity.PostFavour;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 帖子收藏数据库操作
+ *
+ * 
+ * 
+ */
+public interface PostFavourMapper extends BaseMapper<PostFavour> {
+
+    /**
+     * 分页查询收藏帖子列表
+     *
+     * @param page
+     * @param queryWrapper
+     * @param favourUserId
+     * @return
+     */
+    Page<Post> listFavourPostByPage(IPage<Post> page, @Param(Constants.WRAPPER) Wrapper<Post> queryWrapper,
+            long favourUserId);
+
+}
+
+
+
+
---
+++ src/main/java/com/wintermist/mbti/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.wintermist.mbti.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wintermist.mbti.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
